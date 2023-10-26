7ddd31b959ce810c3f754ffa470d52aa806139d7
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2023-10-26 23:48:37 +1300

    feat: initial, animate, hover, press and exit animations, along with default transitions and emitting motionStart and motionEnd events

--- src/Motion/index.tsx
+++ src/Motion/index.tsx
@@ -1,3 +1,9 @@
+import {
+  type AnimationControls,
+  type AnimationOptionsWithOverrides,
+  type MotionKeyframesDefinition,
+  animate,
+} from "motion";
 import React from "react";
 
 export type Motion = {
@@ -11,18 +17,178 @@ export const Motion: Motion = new Proxy(Object.create(null), {
       PolymorphicMotion({
         as,
         ...props,
-      } as unknown as PolymorphicMotionProps<T>),
+      } as PolymorphicMotionProps<T>),
 });
 
 type PolymorphicMotionProps<T extends keyof JSX.IntrinsicElements> = {
   as: T;
+  initial?: KeyframesDefinition;
+  animate?: KeyframesDefinition;
+  hover?: KeyframesDefinition;
+  press?: KeyframesDefinition;
+  exit?: KeyframesDefinition;
+  transition?: AnimationOptionsWithOverrides;
+  onMotionStart?: (controls: AnimationControls) => void;
+  onMotionEnd?: (controls: AnimationControls) => void;
 } & JSX.IntrinsicElements[T];
+
+interface KeyframesDefinition extends MotionKeyframesDefinition {
+  transition?: AnimationOptionsWithOverrides;
+}
 
 const PolymorphicMotion = <T extends keyof JSX.IntrinsicElements>({
   as,
+  initial: initialAnimations,
+  animate: alwaysAnimations,
+  hover: hoverAnimations,
+  press: pressAnimations,
+  exit: exitAnimations,
+  transition,
+  onMouseOver,
+  onClick,
+  onMotionStart,
+  onMotionEnd,
   ...rest
 }: PolymorphicMotionProps<T>) => {
+  const haveInitialAnimationsTriggered = React.useRef(!initialAnimations);
+  const componentRef = React.useRef<null | HTMLElement>(null);
+
+  const emitMotionEvents = React.useCallback(
+    (controls: AnimationControls) => {
+      onMotionStart?.(controls);
+      controls.finished.then(() => onMotionEnd?.(controls));
+    },
+    [onMotionStart, onMotionEnd]
+  );
+
   const Component = as as string;
 
-  return <Component {...rest} />;
+  const onMouseOverWithAnimation = React.useCallback(
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (event: React.MouseEvent<any>) => {
+      onMouseOver?.(event);
+
+      if (!componentRef.current || !hoverAnimations) return;
+
+      const { transition: hoverAnimationsTransitions, ...rest } =
+        hoverAnimations;
+
+      const hoverAnimationsControls = animate(
+        componentRef.current,
+        rest,
+        hoverAnimationsTransitions ?? transition
+      );
+
+      emitMotionEvents(hoverAnimationsControls);
+    },
+    [onMouseOver, hoverAnimations, transition, emitMotionEvents]
+  );
+
+  const onClickWithAnimation = React.useCallback(
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (event: React.MouseEvent<any>) => {
+      onClick?.(event);
+
+      if (!componentRef.current || !pressAnimations) return;
+
+      const { transition: pressAnimationsTransitions, ...rest } =
+        pressAnimations;
+
+      const pressAnimationsControls = animate(
+        componentRef.current,
+        rest,
+        pressAnimationsTransitions ?? transition
+      );
+
+      emitMotionEvents(pressAnimationsControls);
+    },
+    [onClick, pressAnimations, transition, emitMotionEvents]
+  );
+
+  React.useEffect(() => {
+    if (
+      !componentRef.current ||
+      !alwaysAnimations ||
+      !haveInitialAnimationsTriggered.current
+    )
+      return;
+
+    const { transition: alwaysAnimationsTransitions, ...rest } =
+      alwaysAnimations;
+
+    const element = componentRef.current;
+
+    const alwaysAnimationsControls = animate(
+      element,
+      rest,
+      alwaysAnimationsTransitions ?? transition
+    );
+
+    emitMotionEvents(alwaysAnimationsControls);
+
+    return () => {
+      alwaysAnimationsControls.stop();
+
+      if (!element || !exitAnimations || initialAnimations) return;
+
+      const { transition: exitAnimationsTransitions, ...rest } = exitAnimations;
+
+      const exitAnimationsControls = animate(
+        element,
+        rest,
+        exitAnimationsTransitions ?? transition
+      );
+
+      emitMotionEvents(exitAnimationsControls);
+    };
+  });
+
+  React.useEffect(() => {
+    if (
+      !componentRef.current ||
+      !initialAnimations ||
+      haveInitialAnimationsTriggered.current
+    )
+      return;
+
+    const { transition: initialAnimationsTransitions, ...rest } =
+      initialAnimations;
+
+    const element = componentRef.current;
+
+    const initialAnimationsControls = animate(
+      element,
+      rest,
+      initialAnimationsTransitions ?? transition
+    );
+
+    haveInitialAnimationsTriggered.current = true;
+
+    emitMotionEvents(initialAnimationsControls);
+
+    return () => {
+      initialAnimationsControls.stop();
+
+      if (!element || !exitAnimations) return;
+
+      const { transition: exitAnimationsTransitions, ...rest } = exitAnimations;
+
+      const exitAnimationsControls = animate(
+        element,
+        rest,
+        exitAnimationsTransitions ?? transition
+      );
+
+      emitMotionEvents(exitAnimationsControls);
+    };
+  }, [initialAnimations, transition, exitAnimations, emitMotionEvents]);
+
+  return (
+    <Component
+      {...rest}
+      ref={componentRef}
+      onMouseOver={onMouseOverWithAnimation}
+      onClick={onClickWithAnimation}
+    />
+  );
 };
