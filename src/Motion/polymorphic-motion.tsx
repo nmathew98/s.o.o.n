587cffb0226566e63f9a4068ad7e4d78a78431cc
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2023-11-01 20:08:30 +1300

    feat: mouse event animations

--- src/Motion/polymorphic-motion.tsx
+++ src/Motion/polymorphic-motion.tsx
@@ -92,10 +92,107 @@ export const PolymorphicMotion = React.forwardRef(
 			},
 		});
 
+		const onMouseOverAnimation = React.useCallback(async () => {
+			await pendingAnimation?.current;
+
+			const controls = animateEvent({
+				initial,
+				animate,
+				event: hover,
+				defaultTransition: transition,
+			})(componentRef.current);
+
+			pendingAnimation.current = controls?.finished;
+
+			return controls?.finished;
+		}, [initial, animate, hover, transition]);
+
+		const onMouseLeaveAnimation = React.useCallback(async () => {
+			await pendingAnimation?.current;
+
+			const controls = animateEvent({
+				initial,
+				animate,
+				event: hover,
+				defaultTransition: transition,
+				reverse: true,
+			})(componentRef.current);
+
+			pendingAnimation.current = controls?.finished;
+
+			return controls?.finished;
+		}, [initial, animate, hover, transition]);
+
+		const onMouseDownAnimation = React.useCallback(async () => {
+			await pendingAnimation?.current;
+
+			const controls = animateEvent({
+				initial,
+				animate,
+				event: press,
+				defaultTransition: transition,
+			})(componentRef.current);
+
+			pendingAnimation.current = controls?.finished;
+
+			return controls?.finished;
+		}, [initial, animate, press, transition]);
+
+		const onMouseUpAnimation = React.useCallback(async () => {
+			await pendingAnimation?.current;
+
+			const controls = animateEvent({
+				initial,
+				animate,
+				event: press,
+				defaultTransition: transition,
+				reverse: true,
+			})(componentRef.current);
+
+			pendingAnimation.current = controls?.finished;
+
+			return controls?.finished;
+		}, [initial, animate, press, transition]);
+
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+		const combinedOnMouseOver = React.useCallback(
+			invoke(onMouseOver, onMouseOverAnimation),
+			[onMouseOver, onMouseOverAnimation],
+		);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+		const combinedOnMouseLeave = React.useCallback(
+			invoke(onMouseLeave, onMouseLeaveAnimation),
+			[onMouseLeave, onMouseLeaveAnimation],
+		);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+		const combinedOnMouseDown = React.useCallback(
+			invoke(onMouseDown, onMouseDownAnimation),
+			[onMouseDown, onMouseDownAnimation],
+		);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+		const combinedOnMouseUp = React.useCallback(
+			invoke(onMouseUp, onMouseUpAnimation),
+			[onMouseUp, onMouseUpAnimation],
+		);
+
 		React.useImperativeHandle(ref, createHandles);
 
 		const Component = as as React.ElementType;
 
-		return <Component {...rest} ref={setRef} />;
+		return (
+			<Component
+				{...rest}
+				ref={setRef}
+				onMouseOver={combinedOnMouseOver}
+				onMouseLeave={combinedOnMouseLeave}
+				onMouseDown={combinedOnMouseDown}
+				onMouseUp={combinedOnMouseUp}
+			/>
+		);
 	},
 );
+
+const invoke =
+	(...handlers: (((...args: any[]) => any) | undefined)[]) =>
+	(...args: any[]) =>
+		handlers.forEach(handler => handler?.(...args));
